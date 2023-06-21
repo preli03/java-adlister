cba083d874520355223c83cbc619e48ae8b4ded8
Patrick Mireles <patrickm.mireles@gmail.com>
2023-06-21 09:30:36 -0500

    cookies

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -4,6 +4,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.file.FileStore;
+import java.util.List
+
+import static java.lang.System.out;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
@@ -12,6 +16,7 @@ public class LoginServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
@@ -19,8 +24,15 @@ public class LoginServlet extends HttpServlet {
         if (validAttempt) {
             request.getSession().setAttribute("user", username); // Set "user" attribute in session
             response.sendRedirect("/profile");
+            FileStore session;
+            String loggedInUser = (String) session.getAttribute("user");
+            if (loggedInUser != null) {
+                out.println("Welcome"); // Modify UserUtils.getUserFullName to retrieve the full name or appropriate user information
+            }
+%>
         } else {
             response.sendRedirect("/login");
         }
+
     }
 }
